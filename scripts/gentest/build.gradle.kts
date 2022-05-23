a5ba832f18b8694d221e26dc5dcf746d19c292e3
Jiwan <jiwanbhandari@Jiwans-MacBook-Pro.local>
2022-05-23 13:29:03 -0700

    testing gentest

--- scripts/gentest/build.gradle.kts
+++ scripts/gentest/build.gradle.kts
@@ -14,6 +14,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.seleniumhq.selenium:selenium-java:4.1.4")
     testImplementation(kotlin("test"))
 }
 
--- scripts/gentest/src/main/kotlin/Main.kt
+++ scripts/gentest/src/main/kotlin/Main.kt
@@ -1,3 +1,22 @@
-fun main() {
-	println("Hello World!")
+import kotlin.io.path.*;
+
+import org.openqa.selenium.safari.SafariOptions
+import org.openqa.selenium.safari.SafariDriver
+
+fun main(args: Array<String>) {
+	val fixturePath = Path(args[0])
+	if (!fixturePath.exists()) {
+		throw RuntimeException("fixture path '${fixturePath}' is invalid.")
+	}
+	val outDirPath = Path(args[1])
+	if (!outDirPath.exists()) {
+		throw RuntimeException("output path '$outDirPath' is invalid. ")
+	}
+	val options = SafariOptions()
+	val driver = SafariDriver(options)
+
+	driver.get("https://google.com")
+	
+	driver.quit()
+	
 }
