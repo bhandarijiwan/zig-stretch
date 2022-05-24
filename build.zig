cf87af14a99a8a310d591ba2c209f77e08551711
Jiwan <jiwanbhandari@Jiwans-MacBook-Pro.local>
2022-05-24 10:32:45 -0700

    codegen

--- build.zig
+++ build.zig
@@ -12,6 +12,11 @@ pub fn build(b: *std.build.Builder) void {
     const main_tests = b.addTest("src/main.zig");
     main_tests.setBuildMode(mode);
 
+    const gen_test = b.addTest("test/gentest/main.zig");
+    gen_test.setBuildMode(mode);
+    gen_test.addPackage(.{ .name = "stretch" , .path = .{ .path = "src/main.zig" } });
+
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&gen_test.step);
 }
